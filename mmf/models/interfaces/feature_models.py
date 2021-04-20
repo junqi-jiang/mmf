2abe154519f222d4b46db0ae2b4579b046ba3131
C Zhang <chenyu.zhang16@imperial.ac.uk>
2021-04-20 14:37:08 +0100

    torchray support for V*BERT

--- mmf/models/interfaces/feature_models.py
+++ mmf/models/interfaces/feature_models.py
@@ -23,11 +23,10 @@ from tools.scripts.features.frcnn.extract_features_frcnn import FeatureExtractor
 ImageType = Union[Type[Image.Image], str]
 PathType = Union[Type[Path], str]
 BaseModelType = Type[BaseModel]
-
+from mmf.models.interfaces.torchray_visual_interface import * 
 
 class FeatureModelInterface(nn.Module):
-
-    def __init__(self, model: BaseModelType, config: DictConfig, model_name : str):
+    def __init__(self, model: BaseModelType, config: DictConfig, model_name: str):
         super().__init__()
         self.model = model
         self.config = config
@@ -44,7 +43,7 @@ class FeatureModelInterface(nn.Module):
         extra_params = {"data_dir": config.data_dir}
         self.processor_dict = build_processors(config.processors, **extra_params)
 
-    def classify(self, image: ImageType, text: str, zero_image=False, zero_text=False):
+    def classify(self, image: ImageType, text: str, image_tensor = None,zero_image=False, zero_text=False):
         """Classifies a given image and text in it into Hateful/Non-Hateful.
         Image can be a url or a local path or you can directly pass a PIL.Image.Image
         object. Text needs to be a sentence containing all text in the image.
@@ -58,50 +57,71 @@ class FeatureModelInterface(nn.Module):
         Returns:
             {"label": 0, "confidence": 0.56}
         """
-        if isinstance(image, str):
-            if image.startswith("http"):
-                temp_file = tempfile.NamedTemporaryFile()
-                download(image, *os.path.split(temp_file.name), disable_tqdm=True)
-                image = tv_helpers.default_loader(temp_file.name)
-                temp_file.close()
-            else:
-                image = tv_helpers.default_loader(image)
+
+
+
+
+        if image_tensor != None:
+            image_tensor = torch.unsqueeze(image_tensor,0)
+            im_feature_0, im_info_0 = torchRay_feat_extract(image_tensor) #???
+        else:
+            if isinstance(image, str):
+                if image.startswith("http"):
+                    temp_file = tempfile.NamedTemporaryFile()
+                    download(image, *os.path.split(temp_file.name), disable_tqdm=True)
+                    image = tv_helpers.default_loader(temp_file.name)
+                    temp_file.close()
+                else:
+                    image = tv_helpers.default_loader(image)
+            _, _, im_feature_0, im_info_0 = self.feature_extractor.extract_features(
+                image_dir=image, save_single=False
+            )
+
 
         text = self.processor_dict["text_processor"]({"text": text})
-
         sample = Sample()
         sample.text = text["text"]
         if "input_ids" in text:
             sample.update(text)
 
         # extract feature
-        _, _, im_feature_0, im_info_0 = self.feature_extractor.extract_features(image_dir=image, save_single=False)
+        #_, _, im_feature_0, im_info_0 = self.feature_extractor.extract_features(
+        #    image_dir=image, save_single=False
+        #)
 
         # re-format the sample list
         sample_im_info = Sample()
 
         # process the bounding boxes for vilbert
         if self.model_name == "vilbert":
-            bbox = np.array(im_info_0['bbox'])
-            image_w = im_info_0['image_width']
-            image_h = im_info_0['image_height']
+            bbox = np.array(im_info_0["bbox"])
+            image_w = im_info_0["image_width"]
+            image_h = im_info_0["image_height"]
             new_bbox = np.zeros((bbox.shape[0], 5), dtype=bbox.dtype)
 
             new_bbox[:, 0] = bbox[:, 0] / image_w
             new_bbox[:, 1] = bbox[:, 1] / image_h
             new_bbox[:, 2] = (bbox[:, 2]) / image_w
             new_bbox[:, 3] = (bbox[:, 3]) / image_h
-            new_bbox[:, 4] = (bbox[:, 2] - bbox[:, 0]) * (bbox[:, 3] - bbox[:, 1]) / (image_w * image_h)
+            new_bbox[:, 4] = (
+                (bbox[:, 2] - bbox[:, 0])
+                * (bbox[:, 3] - bbox[:, 1])
+                / (image_w * image_h)
+            )
 
             sample_im_info.bbox = torch.from_numpy(new_bbox)
         else:
-            sample_im_info.bbox = torch.from_numpy(np.array(im_info_0['bbox']))
+            sample_im_info.bbox = torch.from_numpy(np.array(im_info_0["bbox"]))
 
-        sample_im_info.num_boxes = torch.from_numpy(np.array(im_info_0['num_boxes']))
-        sample_im_info.objects = torch.from_numpy(np.array(im_info_0['objects']))
-        sample_im_info.image_width = torch.from_numpy(np.array(im_info_0['image_width']))
-        sample_im_info.image_height = torch.from_numpy(np.array(im_info_0['image_height']))
-        sample_im_info.cls_prob = torch.from_numpy(np.array(im_info_0['cls_prob']))
+        sample_im_info.num_boxes = torch.from_numpy(np.array(im_info_0["num_boxes"]))
+        sample_im_info.objects = torch.from_numpy(np.array(im_info_0["objects"]))
+        sample_im_info.image_width = torch.from_numpy(
+            np.array(im_info_0["image_width"])
+        )
+        sample_im_info.image_height = torch.from_numpy(
+            np.array(im_info_0["image_height"])
+        )
+        sample_im_info.cls_prob = torch.from_numpy(np.array(im_info_0["cls_prob"]))
         sample_list_info = SampleList([sample_im_info])
 
         sample.image_feature_0 = im_feature_0
@@ -114,7 +134,10 @@ class FeatureModelInterface(nn.Module):
 
         output = self.model(sample_list)
         scores = nn.functional.softmax(output["scores"], dim=1)
+
+        if image_tensor != None:
+            return scores
+
         confidence, label = torch.max(scores, dim=1)
 
         return {"label": label.item(), "confidence": confidence.item()}
-
--- mmf/models/interfaces/image_models.py
+++ mmf/models/interfaces/image_models.py
@@ -25,7 +25,6 @@ BaseModelType = Type[BaseModel]
 
 
 class GeneralInterface(nn.Module):
-
     def __init__(self, model: BaseModelType, config: DictConfig):
         super().__init__()
         self.model = model
@@ -40,7 +39,14 @@ class GeneralInterface(nn.Module):
         extra_params = {"data_dir": config.data_dir}
         self.processor_dict = build_processors(config.processors, **extra_params)
 
-    def classify(self, image: ImageType, text: str, image_tensor = None, zero_image=False, zero_text=False):
+    def classify(
+        self,
+        image: ImageType,
+        text: str,
+        image_tensor=None,
+        zero_image=False,
+        zero_text=False,
+    ):
         """Classifies a given image and text in it into Hateful/Non-Hateful.
         Image can be a url or a local path or you can directly pass a PIL.Image.Image
         object. Text needs to be a sentence containing all text in the image.
@@ -58,10 +64,10 @@ class GeneralInterface(nn.Module):
         sample = Sample()
 
         if image_tensor != None:
+            #if len(image_tensor.shape) != 4:
+            #    image_tensor = torch.unsqueeze(image_tensor,0)
             sample.image = image_tensor
         else:
-            
-
 
             if isinstance(image, str):
                 if image.startswith("http"):
@@ -72,12 +78,10 @@ class GeneralInterface(nn.Module):
                 else:
                     image = tv_helpers.default_loader(image)
 
-        
             image = self.processor_dict["image_processor"](image)
             sample.image = image
 
         text = self.processor_dict["text_processor"]({"text": text})
-
 
         sample.text = text["text"]
         if "input_ids" in text:
@@ -95,4 +99,3 @@ class GeneralInterface(nn.Module):
         confidence, label = torch.max(scores, dim=1)
 
         return {"label": label.item(), "confidence": confidence.item()}
-
